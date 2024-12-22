39d5a09df8790016a0623fc222cc568c0faf1781
Gishan Abeysinghe <gishan.abeysinghe@gmail.com>
2024-12-23 02:11:54 +0530

    add font awesome

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -1,24 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: true, 
+  ssr: true,
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', 'nuxt-lucide-icons'],
+
   app: {
     head: {
       title: 'Rescape',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
+        }
       ]
     }
   },
+
   runtimeConfig: {
     public: {
       apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8000/api',
       contentBase: process.env.NUXT_PUBLIC_CONTENT_BASE || 'http://localhost:8000/api'
     }
   },
+
   lucide: {
     namePrefix: 'Icon'
-  }
+  },
+
+  compatibilityDate: '2024-12-23'
 })
