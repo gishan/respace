1e0b9c127952de593829fe65d872aedd89ec4a33
Gishan Abeysinghe <gishan.abeysinghe@gmail.com>
2024-12-22 07:56:30 +0530

    add env to nuxt config

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -14,7 +14,8 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8000/api'
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8000/api',
+      contentBase: process.env.NUXT_PUBLIC_CONTENT_BASE || 'http://localhost:8000/api'
     }
   },
   lucide: {
